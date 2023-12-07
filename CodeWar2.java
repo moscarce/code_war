3498a886e663c7edf15ed785adcec1790da90a55
Akinjokun Oluwatobi <125365846+moscarce@users.noreply.github.com>
2023-12-07 17:26:40 +0100

    Add files via upload
    question 2

---
+++ CodeWar2.java
@@ -0,0 +1,20 @@
+public class CodeWar2{
+	public static void main(String[] args){
+		String[] inputList = {"ale", "apple", "monkey", "plea"};
+		String inputWord = "abpcplea";
+	}
+	
+	public static String longestWord(String[] list, String word){
+		String longestWord = "";
+		for(int index = 0;index < list.length; index++){
+			if(wordContains(list[index], word)){
+				if(list[index].length() > longestWord.length()) longestWord = list[index];
+			}	
+		}
+		return longestWord;
+	}
+
+	public static void wordContains(String compareWord,String word){
+		
+	}
+}
